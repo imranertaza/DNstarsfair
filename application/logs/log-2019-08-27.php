0d3079fc999490f07fc127ea01b4935133e59b47
Syed Imran Ertaza <imran@dnationsoft.com>
2019-08-27 17:01:39 +0600

    cached added

---
+++ application/logs/log-2019-08-27.php
@@ -0,0 +1,38 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-08-27 17:01:03 --> Config Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Hooks Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Utf8 Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> UTF-8 Support Enabled
+DEBUG - 2019-08-27 17:01:03 --> URI Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Router Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Output Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Security Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Input Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2019-08-27 17:01:03 --> Language Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Loader Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Helper loaded: url_helper
+DEBUG - 2019-08-27 17:01:03 --> Helper loaded: path_helper
+DEBUG - 2019-08-27 17:01:03 --> Session Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Helper loaded: string_helper
+DEBUG - 2019-08-27 17:01:03 --> Database Driver Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Session routines successfully run
+DEBUG - 2019-08-27 17:01:03 --> Helper loaded: form_helper
+DEBUG - 2019-08-27 17:01:03 --> Form Validation Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Pagination Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Controller Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-08-27 17:01:03 --> Helper loaded: settings_functions_helper
+DEBUG - 2019-08-27 17:01:03 --> Model Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2019-08-27 17:01:03 --> Model Class Initialized
+DEBUG - 2019-08-27 17:01:03 --> Pagination class already loaded. Second attempt ignored.
+DEBUG - 2019-08-27 17:01:03 --> File loaded: application/views/front/sidebar-left.php
+DEBUG - 2019-08-27 17:01:03 --> File loaded: application/views/front/header.php
+ERROR - 2019-08-27 17:01:03 --> Severity: Notice  --> Undefined variable: failed /var/www/html/starsfair/web_v2/application/views/front/member_form.php 10
+ERROR - 2019-08-27 17:01:03 --> Severity: Notice  --> Undefined variable: more_same_user /var/www/html/starsfair/web_v2/application/views/front/member_form.php 10
+DEBUG - 2019-08-27 17:01:03 --> File loaded: application/views/front/member_form.php
+DEBUG - 2019-08-27 17:01:03 --> File loaded: application/views/front/footer.php
+DEBUG - 2019-08-27 17:01:03 --> Final output sent to browser
+DEBUG - 2019-08-27 17:01:03 --> Total execution time: 0.0114
