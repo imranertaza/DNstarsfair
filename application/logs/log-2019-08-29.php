7402a25c1ca2db5d2f731b28250a4fe48ad7a63d
Syed Imran Ertaza <imran@dnationsoft.com>
2019-08-29 10:02:35 +0600

    added log file

---
+++ application/logs/log-2019-08-29.php
@@ -0,0 +1,39 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-08-29 10:00:40 --> Config Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Hooks Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Utf8 Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> UTF-8 Support Enabled
+DEBUG - 2019-08-29 10:00:40 --> URI Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Router Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> No URI present. Default controller set.
+DEBUG - 2019-08-29 10:00:40 --> Output Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Security Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Input Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Global POST and COOKIE data sanitized
+DEBUG - 2019-08-29 10:00:40 --> Language Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Loader Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Helper loaded: url_helper
+DEBUG - 2019-08-29 10:00:40 --> Helper loaded: path_helper
+DEBUG - 2019-08-29 10:00:40 --> Session Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Helper loaded: string_helper
+DEBUG - 2019-08-29 10:00:40 --> Database Driver Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Session routines successfully run
+DEBUG - 2019-08-29 10:00:40 --> Helper loaded: form_helper
+DEBUG - 2019-08-29 10:00:40 --> Form Validation Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Pagination Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Controller Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-08-29 10:00:40 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2019-08-29 10:00:40 --> Helper loaded: settings_functions_helper
+DEBUG - 2019-08-29 10:00:40 --> Pagination class already loaded. Second attempt ignored.
+DEBUG - 2019-08-29 10:00:40 --> Model Class Initialized
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/slider.php
+ERROR - 2019-08-29 10:00:40 --> Severity: Notice  --> Undefined variable: check_user /var/www/html/starsfair/web_v2/application/views/front/sidebar-left.php 23
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/sidebar-left.php
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/sidebar-left.php
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/header.php
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/page.php
+DEBUG - 2019-08-29 10:00:40 --> File loaded: application/views/front/footer.php
+DEBUG - 2019-08-29 10:00:40 --> Final output sent to browser
+DEBUG - 2019-08-29 10:00:40 --> Total execution time: 0.0096
